b873029cd16846f405483316c1e7236fa82b9766
vitorgatti <vitormgatti@gmail.com>
2021-09-07 20:43:03 -0300

    cadastro de usuario

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css'
 
+import 'firebase/auth';
+import firebase from '../../config/firebase';
+
 function Login(){
+  const  [email, setEmail] = useState();
+  const  [senha, setSenha] = useState();
+  const  [msg, setMsgTipo] = useState();
+
+  function logar(){
+    firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
+      setMsgTipo('sucesso');
+    }).catch(erro => {
+      setMsgTipo('erro');
+    });
+  }
+
   return (
     <div className="login-content d-flex align-itens-center">    
       <form className="form-signin mx-auto">
@@ -9,15 +24,14 @@ function Login(){
         <h1 className="h3 mb-3 text-white fw-bold">Login</h1>
       </div>      
         
-        <input type="email" class="form-control my-2" id="inputEmail" placeholder="Email" />        
-        <input type="password" class="form-control my-2" id="floatingPassword" placeholder="Senha" />
+        <input onChange={(e) => setEmail(e.target.value)} type="email" class="form-control my-2" id="inputEmail" placeholder="Email" />        
+        <input onChange={(e) => setSenha(e.target.value)} type="password" class="form-control my-2" id="floatingPassword" placeholder="Senha" />
         
-        <button class="w-100 btn btn-lg btn-block btn-login" type="submit">Sign in</button>
+        <button onClick={logar} class="w-100 btn btn-lg btn-block btn-login" type="button">Logar</button>
 
         <div className="msg-login text-white text-center my-5 text-center">
-          <span><strong>Wow!</strong> Você está conectado! &#128526;</span>
-          <br/>
-          <span><strong>Wow!</strong> Verifique se a senha ou usuário estão corretos! &#128546;</span>
+            {msg === 'sucesso' && <span><strong>Wow!</strong> Você está conectado! &#128526;</span>}
+            {msg === 'erro' && <span><strong>Wow!</strong> Verifique se a senha ou usuário estão corretos! &#128546;</span>}
         </div>
 
       <div className="opcoes-login mt-5 text-center">
---
+++ src/view/usuario-novo/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+import firebase from '../../config/firebase';
+import 'firebase/auth';
+
+import './usuario-novo.css'
+
+function NovoUsuario(){
+  const  [email, setEmail] = useState();
+  const  [senha, setSenha] = useState();
+  const  [msgTipo, setMsgTipo] = useState();
+  const  [msg, setMsg] = useState();
+  const  [carregando, setCarregando] = useState();
+
+  function Cadastrar(){
+    setCarregando(1);
+    setMsg(null);
+
+    if(!email || !senha) {
+      setMsgTipo('erro')
+      setMsg('É necessário informar o e-mail e senha para fazer o cadastro!')
+      return;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, senha).then(resultado => 
+      {
+        setCarregando(0);
+        setMsgTipo('sucesso')
+      }).catch (erro =>  {
+        setCarregando(0);
+        setMsgTipo('erro')
+        setMsg(erro.message);
+      });
+  }
+
+  return (
+    <div className="form-cadastro">
+      <form className="text-center form-login mx-auto mt-5">
+        <h1 className="h3 mb-3 text-black fw-bold"> Cadastro </h1>
+        
+        <input onChange={(e) => setEmail(e.target.value)} className="form-control my-2" type="email" placeholder="Email"></input>
+        <input onChange={(e) => setSenha(e.target.value)} className="form-control my-2" type="password" placeholder="Senha"></input>
+
+        {
+          carregando ? <div class="spinner-border text-danger mt-5" role="status"><span class="sr-only"></span></div> :
+          <button onClick={Cadastrar} type="button" className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro">Cadastrar</button>
+        }
+        
+        <div className="msg-login text-black text-center my-5 text-center">
+            {msgTipo === 'sucesso' && <span><strong>Wow!</strong> Usuário cadastrado com sucesso! &#128526;</span>}
+            {msgTipo === 'erro' && <span><strong>Ops!</strong> {msg} &#128546;</span>}
+        </div>
+        
+      </form>
+    </div>
+  )
+}
+
+export default NovoUsuario;
