086d8c2d8883f5d8638a3e546076c35b75bf06fc
vitorgatti <vitormgatti@gmail.com>
2021-09-12 14:15:12 -0300

    add evento componet

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,11 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './home.css';
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar';
+import firebase from '../../config/firebase'
+import EventoCard from '../../components/evento-card';
 
 function Home(){
+  const [eventos, setEventos] = useState([]);
+  let listaEventos = [];
+
+  useEffect(() => {
+
+    firebase.firestore().collection('eventos').get().then(async (resultado) => {
+      await resultado.docs.forEach(doc => {
+        listaEventos.push({
+          id:doc.id,
+          ...doc.data()
+        });
+      });
+
+      setEventos(listaEventos);
+    });
+  });
+
   return(
-    <Navbar></Navbar>
+    <>
+      <Navbar/>
+
+      <div className="row p-3">
+        {eventos.map(item => <EventoCard id={item.id} img={item.foto} titulo={item.detalhes} visualizacoes={item.visualizacoes}/>) }
+      </div>
+    </>
   )
 }
 
